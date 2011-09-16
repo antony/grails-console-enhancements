7154d778fb734535040b0f9d01ffb6c7743569d4
Antony Jones <ant@interrobang.(none)>
2011-09-16 17:28:03 +0100

    Println object

---
+++ src/groovy/uk/co/desirableobjects/console/enhancements/ANSIPrintLn.groovy
@@ -0,0 +1,17 @@
+package uk.co.desirableobjects.console.enhancements
+
+class ANSIPrintLn {
+
+    def methodMissing(String name, args) {
+        List<ANSICode> colourName = [ANSICode.valueOf(ANSICode.class, "FOREGROUND_${name.toUpperCase()}")]
+        String output = new ANSISequence(colourName).format(args[0])
+        return output
+    }
+
+    String printLine(String message) {
+
+        
+
+    }
+
+}
