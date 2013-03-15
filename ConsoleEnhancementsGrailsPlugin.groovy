ad7a2108989f34319248386e005039f4a4d39c56
popcorn <blah@blah.org>
2013-03-15 17:13:04 +0000

    Enhance test output on console.

--- ConsoleEnhancementsGrailsPlugin.groovy
+++ ConsoleEnhancementsGrailsPlugin.groovy
@@ -1,17 +1,14 @@
 import uk.co.desirableobjects.console.enhancements.ConsoleEnhancer
-import uk.co.desirableobjects.console.enhancements.ANSICode
 
 class ConsoleEnhancementsGrailsPlugin {
-    // the plugin version
-    def version = "0.4"
-    // the version or versions of Grails the plugin is designed for
+
+    def version = "1.0"
     def grailsVersion = "1.3.1 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
+	def observe = ["*"]
 
     // TODO Fill in these fields
     def author = "Antony Jones (Desirable Objects)"
@@ -19,35 +16,14 @@ class ConsoleEnhancementsGrailsPlugin {
     def title = "Grails console enhancements"
     def description = 'Enhances the grails console output for better visibility'
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/grails-console-enhancements"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
     def doWithDynamicMethods = { ctx ->
-
         ConsoleEnhancer.enhanceConsole()
-
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
+	    ConsoleEnhancer.enhanceConsole()
     }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
